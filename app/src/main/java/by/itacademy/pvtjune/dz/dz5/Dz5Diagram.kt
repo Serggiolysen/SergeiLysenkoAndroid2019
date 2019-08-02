bd112f08b370a58748a60768984bf6e0234f69ab
SerggioLysenko <serggio88888@gmail.com>
2019-08-02 17:02:58 +0300

    3 files fro dz5 added on dz5  branch

--- app/src/main/java/by/itacademy/pvtjune/dz/dz5/Dz5Diagram.kt
+++ app/src/main/java/by/itacademy/pvtjune/dz/dz5/Dz5Diagram.kt
@@ -1,16 +1,16 @@
-// package by.itacademy.pvtjune.dz.dz5
-//
-// import android.app.Activity
-// import android.os.Bundle
-// import android.util.Log
-// import by.itacademy.myapplication.R
-//
-// class Dz5Diagram : Activity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_dz5_diagram)
-//
-//        Log.d("TAGGG", "onCreate")
-//    }
-// }
+package by.itacademy.pvtjune.dz.dz5
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import by.itacademy.myapplication.R
+
+class Dz5Diagram : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz5_diagram)
+
+        Log.d("TAGGG", "onCreate")
+    }
+}
