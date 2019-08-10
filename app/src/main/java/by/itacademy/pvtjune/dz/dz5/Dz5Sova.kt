00ff4cdfe13213dada4d8aee75f6a0d2ab4fdaab
SerggioLysenko <serggio88888@gmail.com>
2019-08-10 18:56:33 +0300

    dz5-sova pull request comments executed

--- app/src/main/java/by/itacademy/pvtjune/dz/dz5/Dz5Sova.kt
+++ app/src/main/java/by/itacademy/pvtjune/dz/dz5/Dz5Sova.kt
@@ -3,18 +3,15 @@ package by.itacademy.pvtjune.dz.dz5
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import by.itacademy.myapplication.R
 import kotlinx.android.synthetic.main.activity_dz5_1.*
 
 class Dz5Sova : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz5_1)
 
         (sova.background as AnimationDrawable).start()
-
     }
 }
